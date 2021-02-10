7b7c505258b2c8a0a180548875c587f6116c35b9
Daniel Pollak <>
2021-02-10 14:58:45 +0200

    change main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from node_worker import NodeGenericWorker
 def main():
     nodes_list = list(parse_nodes_config(r'tests\files\nodes1.config'))
     feeder = FileFeeder(r"tests\files\tasks1.config")
-    
+
     loadbalancer = RRLoadBalancer(nodes_list)
     dispatcher = TaskDispathcer(feeder, loadbalancer)
 
@@ -18,7 +18,7 @@ def main():
         worker.start()
 
     dispatcher.dispatch_tasks()
-
+    
     for worker in workers_list:
         worker.join()
 
